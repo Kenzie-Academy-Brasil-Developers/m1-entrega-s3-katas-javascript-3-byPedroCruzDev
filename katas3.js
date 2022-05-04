e64f6d2af6ada2ce56b6177f9d9769fd8321b253
[Pedro Cruz] <contatocreed67@gmail.com>
2022-05-04 21:46:38 -0200

    ok

--- katas3.js
+++ katas3.js
@@ -1,97 +1,223 @@
 const sampleArray = [469, 755, 244, 245, 758, 450, 302, 17, 712, 71, 456, 21, 398, 339, 882, 848, 179, 535, 940, 472];
 
-function kata1() {
-    // implemente o código do kata 1 aqui
+function kata1(numbers){
+    
+    let count = [];
+    for(let index=1; index <= numbers; index++){
+        count.push(index)
+    } return count
 }
+console.log(kata1(25))
 
-function kata2() {
-    // implemente o código do kata 2 aqui
-}
+//------------------------------
 
-function kata3() {
-    // implemente o código do kata 3 aqui
-}
+function kata2(numbers){
 
-function kata4() {
-    // implemente o código do kata 4 aqui
+    let count = [];
+    for(let index = 25; index>=numbers; index --){
+        count.push(index)
+    }return count
 }
+console.log(kata2(1))
 
-function kata5() {
-    // implemente o código do kata 5 aqui
-}
+//------------------------------
 
-function kata6() {
-    // implemente o código do kata 6 aqui
-}
+function kata3(numbers){
 
-function kata7() {
-    // implemente o código do kata 7 aqui
+    let count = [];
+    for(let index = -1; index>=numbers; index --){
+        count.push(index)
+    }return count
 }
+console.log(kata3(-25))
 
-function kata8() {
-    // implemente o código do kata 8 aqui
-}
+//------------------------------
 
-function kata9() {
-    // implemente o código do kata 9 aqui
+function kata4(numbers) {
+   
+    let counter = [];
+
+    for(let index = -25; index <= numbers; index++){
+        counter.push(index)
+    }return counter
 }
+console.log(kata4(-1))
+
+//------------------------------
+
+function kata5(numbers) {
+
+    let counter = [];
+    for(let index = 25; index >= numbers; index --){
+
+        if(index % 2 !== 0){
+            counter.push(index)
+        }
+    }return counter
+}
+console.log(kata5(-25))
+
+//------------------------------
+
+function kata6(numbers) {
+    
+    let counter=[];
+
+    for(let index = 1; index < numbers; index++){
+        if(index % 3 === 0){
+            counter.push(index)
+        }
+    }return counter
+}
+console.log(kata6(100))
+
+//------------------------------
+
+function kata7(numbers) {
+    
+    let counter =[];
+
+    for(let index = 1; index <= numbers; index ++){
+
+        if(index % 7 === 0) counter.push(index)
+    }return counter
+}
+console.log(kata7(100))
+
+//-------------------------------
+
+function kata8(numbers) {
+    
+    let counter =[];
+
+    for(let index = 1; index <= numbers; index ++){
+
+        if(index % 7 === 0 || index % 3 === 0) counter.push(index)
+    }return counter
+}
+console.log(kata8(100))
+
+//-------------------------------
+
+function kata9(numbers) {
+
+    let counter = [];
+
+    for(let index = 1; index <= numbers; index++){
+
+        if(index % 5 === 0 && index % 2 !== 0){
+            counter.push(index)
+        }
+    }return counter
+}
+console.log(kata9(100))
+
+//-----------------------------------
 
 function kata10() {
-    // implemente o código do kata 10 aqui
+
+    let counter = [];
+    for(let index=0; index < sampleArray.length; index++){
+        counter.push(sampleArray[index])
+    }return counter
 }
+console.log(kata10())
+
+//--------------------------------
 
 function kata11() {
-    // implemente o código do kata 11 aqui
+    
+    let counter = [];
+    for(let index=0; index < sampleArray.length; index++){
+        if(sampleArray[index] % 2 === 0){
+            counter.push(sampleArray[index])
+        }
+    }return counter
 }
+console.log(kata11())
+
+//-------------------------------------
 
 function kata12() {
-    // implemente o código do kata 12 aqui
+    
+    let counter = [];
+    for(let index=0; index < sampleArray.length; index++){
+        if(sampleArray[index] % 2 !== 0){
+            counter.push(sampleArray[index])
+        }
+    }return counter
 }
+console.log(kata12())
+
+//----------------------------------------
 
 function kata13() {
-    // implemente o código do kata 13 aqui
+    
+    let counter = [];
+
+    for(let index = 0; index < sampleArray.length; index ++){
+        if(sampleArray[index]%8===0) counter.push(sampleArray[index])
+    }return counter
 }
+console.log(kata13())
 
 function kata14() {
-    // implemente o código do kata 14 aqui
-}
 
-function kata15() {
-    // implemente o código do kata 15 aqui
+    let counter = [];
+    for(let index=0; index < sampleArray.length; index ++){
+        counter.push(sampleArray[index] ** 2)
+    }
+        return counter
 }
+console.log(kata14())
+
+//------------------
+
+function kata15(numbers) {
+
+    let counter=0;
+    for(let index = 0; index < numbers; index++){
+        counter+= index
+    }
+        return counter 
+}
+console.log(kata15(20))
+//--------------------------------
 
 function kata16() {
-    // implemente o código do kata 16 aqui
+
+    let counter=0;
+    for(let index=0; index < sampleArray.length; index ++){
+        counter += sampleArray[index]
+    }
+    return counter ++
 }
+console.log(kata16())
 
 function kata17() {
-    // implemente o código do kata 17 aqui
+
+    let counter=0;
+    for(let index=0; index < sampleArray.length; index ++){
+        counter = Math.min(...sampleArray)
+
+    }
+        return counter
 }
+console.log(kata17())
+
+//------------------------------------------------
 
 function kata18() {
-    // implemente o código do kata 18 aqui
+    let counter = [];
+
+    for(let index = 0; index < sampleArray.length; index++){
+        counter = Math.max(...sampleArray)
+
+    }
+        return counter
 }
 
-/**
- * Daqui em diante são os bônus, por sua conta e risco
- */
+console.log(kata18())
 
-function kataBonus1() {
-    // implemente o código do kata bonus 1 aqui
-}
+//não consegui achar uma meneira de achar a maximo e o minimo valor, sem o Math, caso haja uma maniera gostaria de saber
 
-function kataBonus2() {
-    // implemente o código do kata bonus 2 aqui
-}
-
-function kataBonus3() {
-    // implemente o código do kata bonus 3 aqui
-}
-
-function kataBonus4() {
-    // implemente o código do kata bonus 4 aqui
-}
-
-function kataBonus5() {
-    // implemente o código do kata bonus 5 aqui
-}
