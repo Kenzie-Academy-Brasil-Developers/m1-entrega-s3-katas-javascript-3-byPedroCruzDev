6e2c417734426ccf65af6ded81d5cc9d43e2568e
[Pedro Cruz] <contatocreed67@gmail.com>
2022-07-01 07:05:06 -0400

    csv incluse

--- katas3.js
+++ katas3.js
@@ -1,24 +1,28 @@
-const sampleArray = [469, 755, 244, 245, 758, 450, 302, 17, 712, 71, 456, 21, 398, 339, 882, 848, 179, 535, 940, 472];
+const sampleArray = [469, 755, 244, 245, 758, 450, 302, 17, 712, 71,
+                    456, 21, 398, 339, 882, 848, 179, 535, 940, 472];
 
 function kata1(numbers){
     
     let count = [];
     for(let index=1; index <= numbers; index++){
-        count.push(index)
+       
+        count.push(index) 
+        
     } return count
 }
-console.log(kata1(25))
+console.log("kata1:" +kata1(25))
 
 //------------------------------
 
-function kata2(numbers){
+ function kata2(numbers){
 
     let count = [];
-    for(let index = 25; index>=numbers; index --){
+    for(let index = numbers; index > 0; index --){
         count.push(index)
+    
     }return count
 }
-console.log(kata2(1))
+console.log("kata2:"+ kata2(25))
 
 //------------------------------
 
@@ -27,35 +31,37 @@ function kata3(numbers){
     let count = [];
     for(let index = -1; index>=numbers; index --){
         count.push(index)
+        
     }return count
 }
-console.log(kata3(-25))
-
+console.log("kata3:"+ kata3(-25))
+ 
 //------------------------------
 
 function kata4(numbers) {
    
     let counter = [];
 
-    for(let index = -25; index <= numbers; index++){
+    for(let index = numbers ; index < 0; index++){
         counter.push(index)
+        
     }return counter
 }
-console.log(kata4(-1))
+console.log("kata4:"+kata4(-25))
 
 //------------------------------
 
 function kata5(numbers) {
 
     let counter = [];
-    for(let index = 25; index >= numbers; index --){
+    for(let index = numbers; index >= -25; index --){
 
         if(index % 2 !== 0){
             counter.push(index)
         }
     }return counter
 }
-console.log(kata5(-25))
+console.log("kata5:"+kata5(25))
 
 //------------------------------
 
@@ -69,7 +75,7 @@ function kata6(numbers) {
         }
     }return counter
 }
-console.log(kata6(100))
+console.log("kata6:"+kata6(100))
 
 //------------------------------
 
@@ -82,7 +88,7 @@ function kata7(numbers) {
         if(index % 7 === 0) counter.push(index)
     }return counter
 }
-console.log(kata7(100))
+console.log("kata7:"+kata7(100))
 
 //-------------------------------
 
@@ -90,12 +96,12 @@ function kata8(numbers) {
     
     let counter =[];
 
-    for(let index = 1; index <= numbers; index ++){
+    for(let index = numbers; index > 0; index --){
 
         if(index % 7 === 0 || index % 3 === 0) counter.push(index)
     }return counter
 }
-console.log(kata8(100))
+console.log("kata8:"+kata8(100))
 
 //-------------------------------
 
@@ -110,114 +116,106 @@ function kata9(numbers) {
         }
     }return counter
 }
-console.log(kata9(100))
+
+console.log("kata9:"+kata9(100))
 
 //-----------------------------------
 
-function kata10() {
+function kata10(arr) {
 
     let counter = [];
-    for(let index=0; index < sampleArray.length; index++){
-        counter.push(sampleArray[index])
+    for(let index=0; index < arr.length; index++){
+        counter.push(arr[index])
     }return counter
 }
-console.log(kata10())
+console.log("kata10:"+kata10(sampleArray))
 
 //--------------------------------
 
-function kata11() {
+function kata11(arr) {
     
     let counter = [];
-    for(let index=0; index < sampleArray.length; index++){
-        if(sampleArray[index] % 2 === 0){
-            counter.push(sampleArray[index])
+    for(let index=0; index < arr.length; index++){
+        if(arr[index] % 2 === 0){
+            counter.push(arr[index])
         }
     }return counter
 }
-console.log(kata11())
+console.log("kata11:"+kata11(sampleArray))
 
 //-------------------------------------
 
-function kata12() {
+function kata12(arr) {
     
     let counter = [];
-    for(let index=0; index < sampleArray.length; index++){
-        if(sampleArray[index] % 2 !== 0){
-            counter.push(sampleArray[index])
+    for(let index=0; index < arr.length; index++){
+        if(arr[index] % 2 !== 0){
+            counter.push(arr[index])
         }
     }return counter
 }
-console.log(kata12())
+console.log("kata12:"+kata12(sampleArray))
 
 //----------------------------------------
 
-function kata13() {
+function kata13(arr) {
     
     let counter = [];
 
-    for(let index = 0; index < sampleArray.length; index ++){
-        if(sampleArray[index]%8===0) counter.push(sampleArray[index])
+    for(let index = 0; index < arr.length; index ++){
+        if(arr[index]%8===0) 
+        counter.push(arr[index])
     }return counter
 }
-console.log(kata13())
+console.log("kata13:"+kata13(sampleArray))
 
-function kata14() {
+function kata14(arr) {
 
     let counter = [];
-    for(let index=0; index < sampleArray.length; index ++){
-        counter.push(sampleArray[index] ** 2)
+    for(let index=0; index < arr.length; index ++){
+        counter.push(arr[index] ** 2)
     }
         return counter
 }
-console.log(kata14())
+console.log("kata14:"+kata14(sampleArray))
 
 //------------------
 
 function kata15(numbers) {
 
     let counter=0;
+
     for(let index = 0; index < numbers; index++){
         counter+= index
     }
         return counter 
 }
-console.log(kata15(20))
+console.log("kata15:"+kata15(20))
 //--------------------------------
 
-function kata16() {
+function kata16(arr) {
 
     let counter=0;
-    for(let index=0; index < sampleArray.length; index ++){
-        counter += sampleArray[index]
+    for(let index=0; index < arr.length; index ++){
+        counter += arr[index]
     }
     return counter ++
 }
-console.log(kata16())
+console.log("kata16:"+kata16(sampleArray))
 
-function kata17() {
-
-    let counter=0;
-    for(let index=0; index < sampleArray.length; index ++){
-        counter = Math.min(...sampleArray)
-
-    }
-        return counter
+function kata17(arr) {
+   return Math.min(...arr)
 }
-console.log(kata17())
+console.log("kata17:"+kata17(sampleArray))
 
 //------------------------------------------------
 
-function kata18() {
-    let counter = [];
-
-    for(let index = 0; index < sampleArray.length; index++){
-        counter = Math.max(...sampleArray)
-
-    }
-        return counter
+function kata18(arr) {
+    return Math.max(...arr)
+        
 }
 
-console.log(kata18())
+console.log("kata18:"+kata18(sampleArray))
 
-//não consegui achar uma meneira de achar a maximo e o minimo valor, sem o Math, caso haja uma maniera gostaria de saber
+
 
